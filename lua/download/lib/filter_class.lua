e4f3fc46159637477580304d68f0b6ba8167457d
spout <snoopyxdy@gmail.com>
2013-12-20 10:03:30 +0800

    xxf

--- lua/download/lib/filter_class.lua
+++ lua/download/lib/filter_class.lua
@@ -1,14 +1,26 @@
 module("filter_class", package.seeall)
 
+string.split = function(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
 Filter_Class = class('Filter_Class')
 
 --构造函数
 function Filter_Class:initialize()
     self.agent = ngx.req.get_headers()["User-Agent"] or "" --获取用户agent
+    local xff = ngx.req.get_headers()["X-Forwarded-For"]
+    if xff then
+	xff = string.split(xff, ',')[1]
+    end
     local args = ngx.req.get_uri_args()
     self.name = ngx.unescape_uri(args["name"] or "")  --获取用户参数name
     self.id = args["id"] or ""       --获取用户参数id
-    self.ip = ngx.var.remote_addr or ""                --获取ip地址
+    self.ip =  xff or ngx.var.remote_addr or ""                --获取ip地址
     self.tag = ngx.unescape_uri(args["tag"] or "")     --获取特殊标识
     self:getDevice()
 
